3a68f1ea59e6628f5f48d7bc04eda3649ac8661c
Bryan Larsen <bryan@larsen.st>
2025-10-13 14:06:49 -0400

    Use Message in Lookup instead of [Record]
    Fixes #2871
    
    In Lookup, replace the `records: Arc<[Record]>` field with `message:
    Arc<Message>`. This allows users of the Lookup, such as the
    `resolve` util to print section information. It also ensures that
    section information is not lost when using hickory as a resolver.
    
    Complications:
    
    - handle_noerror changed considerably. I've assumed the SRV record
    special casing that was previously in the function was due to records
    moving from other sections into answers, so that special casing is no
    longer necessary.
    
    - many of the functions (like iter) now use all_sections under the
    hood for backwards compatibility, even though most users likely want
    answers(). And because all_sections() is an iterator over several
    lists, this is less efficient.

--- crates/proto/src/dnssec/mod.rs
+++ crates/proto/src/dnssec/mod.rs
@@ -7,6 +7,7 @@
 
 //! dns security extension related modules
 
+use alloc::boxed::Box;
 use alloc::string::String;
 use alloc::vec::Vec;
 use core::fmt;
@@ -64,6 +65,42 @@ pub use tsig::{TSigResponseContext, TSigner};
 
 mod verifier;
 pub use verifier::Verifier;
+
+/// An iterator over record data with all data wrapped in a Proven type for dnssec validation
+pub struct DnssecIter<'a>(DnssecRecordIter<'a>);
+
+impl<'a> DnssecIter<'a> {
+    /// Create a new DnssecIter from any iterator of Record references
+    pub fn new(iter: impl Iterator<Item = &'a Record> + 'a) -> Self {
+        Self(DnssecRecordIter::new(iter))
+    }
+}
+
+impl<'a> Iterator for DnssecIter<'a> {
+    type Item = Proven<&'a crate::rr::RData>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().map(|r| r.map(Record::data))
+    }
+}
+
+/// An iterator over records with all records wrapped in a Proven type for dnssec validation
+pub struct DnssecRecordIter<'a>(Box<dyn Iterator<Item = &'a Record> + 'a>);
+
+impl<'a> DnssecRecordIter<'a> {
+    /// Create a new DnssecRecordIter from any iterator of Record references
+    pub fn new(iter: impl Iterator<Item = &'a Record> + 'a) -> Self {
+        Self(alloc::boxed::Box::new(iter))
+    }
+}
+
+impl<'a> Iterator for DnssecRecordIter<'a> {
+    type Item = Proven<&'a Record>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().map(Proven::from)
+    }
+}
 
 /// DNSSEC Delegation Signer (DS) Resource Record (RR) Type Digest Algorithms
 ///
